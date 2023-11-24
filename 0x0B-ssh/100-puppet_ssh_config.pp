5cee188fccd6c05d558f661be00fc8a0278463a8
Githu77 <githubenard16@gmail.com>
2023-11-24 12:25:30 +0300

    adding files

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#configuring ssh config
+
+file_line{'switching off passwordd authentication':
+path => '/etc/ssh/ssh_config',
+line => 'PasswordAuthentication no'
+}
+
+file_line{'File identity':
+path => '/etc/ssh/ssh_config',
+line => 'IdentityFile ~/.ssh/school'
+
+}
